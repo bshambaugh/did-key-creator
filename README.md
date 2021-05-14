427b06382c5ae9ab0e3cbfc37476ff2a93004b42
Brent Shambaugh <brent.shambaugh@gmail.com>
2021-05-14 15:04:47 -0500

    add miscellaneous functions from orginally from secp256r1 resolver links

--- README.md
+++ README.md
@@ -4,8 +4,20 @@ Still work in progress.
 ## Usage
 
 ```js
-import { Secp256r1Provider } from 'key-did-provider-secp256r1'
+import * as key-did-secp256r1 from './key-did-provider-secp256r1/dist/index.js'
+import u8a from 'uint8arrays'
 
+const publicKeyHex = 'f9c36f8964623378bdc068d4bce07ed17c8fa486f9ac0c2613ca3c8c306d7bb61cd36717b8ac5e4fea8ad23dc8d0783c2318ee4ad7a80db6e0026ad0b072a24f';
+const publicKey = u8a.fromString(publicKeyHex,'base16');
+
+console.log(key-did-secp256r1.encodeDID(publicKey));
+
+console.log(key-did-secp256r1.encodeDIDfromHexString(publicKeyHex));
+
+output:
+
+did:key:zruuPojWkzGPb8sVc42f2YxcTXKUTpAUbdrzVovaTBmGGNyK6cGFaA4Kp7SSLKecrxYz8Sc9d77Rss7rayYt1oFCaNJ
+did:key:zruuPojWkzGPb8sVc42f2YxcTXKUTpAUbdrzVovaTBmGGNyK6cGFaA4Kp7SSLKecrxYz8Sc9d77Rss7rayYt1oFCaNJ
 ```
 
 ## License
--- src/index.ts
+++ src/index.ts
@@ -12,5 +12,34 @@ export function encodeDID(publicKey: Uint8Array) {
           const publicKeywPrefix = multicodec.addPrefix('p256-pub',publicKey)
           const buf = multibase.encode('base58btc',publicKeywPrefix)
           const bufAsString = u8a.toString(buf)
-          return `did:key:${bufAsString}`
+          return `did:key:${bufAsString}`	  
+}
+
+// write a separate test for this function...
+// source: https://stackoverflow.com/questions/17171542/algorithm-for-elliptic-curve-point-compression
+export function ECPointCompress( x: Uint8Array, y: Uint8Array )
+{
+    const out = new Uint8Array( x.length + 1 );
+
+    out[0] = 2 + ( y[ y.length-1 ] & 1 );
+    out.set( x, 1 );
+
+    return out;
+}
+
+export function pubKeyHexToUint8Array(publicKeyHex: string) {
+   const publicKey_u8a = u8a.fromString(publicKeyHex,'base16');
+   return publicKey_u8a;
+}
+
+export function compresedKeyInHex(publicKeyHex: string) {
+  const xHex = publicKeyHex.slice(0,publicKeyHex.length/2);
+  const yHex = publicKeyHex.slice(publicKeyHex.length/2,publicKeyHex.length);
+
+  const xOctet = u8a.fromString(xHex,'base16')
+  const yOctet = u8a.fromString(yHex,'base16');
+
+  const compressedPoint = ECPointCompress( xOctet , yOctet );
+  const compressedPointHex = u8a.toString(compressedPoint,'base16');
+  return compressedPointHex;
 }
