b588acce75f934fd115e080e78b909c2bb89a33c
Brent Shambaugh <brent.shambaugh@gmail.com>
2021-06-04 08:05:16 -0500

    fix codecName type and parameter ordering

--- src/encodeDIDkey.ts
+++ src/encodeDIDkey.ts
@@ -1,14 +1,17 @@
 import * as u8a from 'uint8arrays'
 import multicodec from 'multicodec'
 import  multibase from'multibase'
+import { CodecName } from 'multicodec/src/generated-types';
 
-export function encodeDIDfromHexString(publicKeyHex: string, multicodecName: string) {
+
+export function encodeDIDfromHexString(multicodecName: CodecName,publicKeyHex: string) {
     const publicKey = u8a.fromString(publicKeyHex,'base16');
-    const didKey = encodeDIDfromBytes(publicKey,multicodecName);
+    const didKey = encodeDIDfromBytes(multicodecName,publicKey);
     return didKey;
 }
 
-export function encodeDIDfromBytes(publicKey: Uint8Array, multicodecName: string) {
+
+export function encodeDIDfromBytes(multicodecName: CodecName, publicKey: Uint8Array) {
           const publicKeywPrefix = multicodec.addPrefix(multicodecName,publicKey)
           const buf = multibase.encode('base58btc',publicKeywPrefix)
           const bufAsString = u8a.toString(buf)
