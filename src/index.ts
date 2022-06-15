f1d00e8ceae72add135aa5f2a910700a5ae445d2
Brent Shambaugh <brent.shambaugh@gmail.com>
2022-06-15 17:59:22 -0500

    update exports and move to version 0.1.4

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
+import { compressedKeyInHexfromRaw, 
+  uncompressedKeyInHexfromRaw, 
+  rawKeyInHexfromUncompressed, 
+  pubKeyHexToUint8Array, 
+  didKeyIDtoPubKeyHex, 
+  didKeyURLtoPubKeyHex } from './did_key_utils'
+
+import { encodeDIDfromHexString, encodeDIDfromBytes } from './encodeDIDkey'
 
 export { compressedKeyInHexfromRaw, 
          uncompressedKeyInHexfromRaw, 
          rawKeyInHexfromUncompressed, 
          pubKeyHexToUint8Array, 
          didKeyIDtoPubKeyHex, 
-         didKeyURLtoPubKeyHex 
-       } from './did_key_utils'
-
-export { encodeDIDfromHexString, encodeDIDfromBytes } from './encodeDIDkey'
+         didKeyURLtoPubKeyHex,
+         encodeDIDfromHexString, 
+         encodeDIDfromBytes
+       } 
