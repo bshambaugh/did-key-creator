79b1b2d4e2e893c9d6f94e323719466c94dd3499
Brent Shambaugh <brent.shambaugh@gmail.com>
2022-06-15 14:53:08 -0500

    update to multiformats

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+// import functions from did_key_utils and encodeDIDkey to export them from here
+import * as encodeDIDkey from './encodeDIDkey'
+import * as did_key_utils from './did_key_utils'
+
+export default { encodeDIDkey, did_key_utils }
