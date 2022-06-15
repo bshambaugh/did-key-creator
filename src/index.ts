45f11399a96a01509cad8197bfe2d986e61043d6
Brent Shambaugh <brent.shambaugh@gmail.com>
2022-06-15 17:37:00 -0500

    fix exports on src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-// import functions from did_key_utils and encodeDIDkey to export them from here
-import * as encodeDIDkey from './encodeDIDkey'
-import * as did_key_utils from './did_key_utils'
 
-export default { encodeDIDkey, did_key_utils }
+export { compressedKeyInHexfromRaw, 
+         uncompressedKeyInHexfromRaw, 
+         rawKeyInHexfromUncompressed, 
+         pubKeyHexToUint8Array, 
+         didKeyIDtoPubKeyHex, 
+         didKeyURLtoPubKeyHex 
+       } from './did_key_utils'
+
+export { encodeDIDfromHexString, encodeDIDfromBytes } from './encodeDIDkey'
